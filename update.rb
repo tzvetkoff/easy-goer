d916394e1223b22f3b745aa86f85ebae9a4edcff
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2020-02-14 14:51:54 +0200

    Update update.rb

--- update.rb
+++ update.rb
@@ -7,42 +7,31 @@ Bundler.require(:default, ENV['RACK_ENV'] || 'development')
 require 'fileutils'
 require 'net/http'
 
+$red, $green, $blue, $yellow, $reset = "\e[1;31m", "\e[1;32m", "\e[1;34m", "\e[1;33m", "\e[0;0m"
+
+def msg(message, color = $reset)
+  puts("#{color}#{message}#{$reset}")
+  yield if block_given?
+end
+
 root = File.expand_path(__dir__)
 
 if File.exist?("#{root}/tmp/dl.html")
+  msg("Using cache: #{root}/tmp/dl.html", $yellow)
   body = File.read("#{root}/tmp/dl.html")
 else
+  msg("Fetching https://golang.org/dl/", $green)
   body = Net::HTTP.get(URI.parse('https://golang.org/dl/'))
 end
 
 doc = Nokogiri::HTML(body)
 
-versions = doc.xpath('//a[starts-with(@href, "https://dl.google.com/go/")]/parent::td/parent::tr').to_a
-versions.map! do |tr|
+new_versions = doc.xpath('//a[starts-with(@href, "https://dl.google.com/go/")]/parent::td/parent::tr').to_a
+new_versions.map! do |tr|
   url = tr.xpath('./td/a').first.attribute('href').value
   next unless url
 
-  checksum = tr.xpath('./td/tt/text()').first.to_s
-  next unless checksum
-
-  {
-    url:      url,
-    checksum: checksum,
-  }
-end
-
-versions.reject! { |version| version[:url].include?('-bootstrap-') }
-versions.reject! { |version| version[:url].include?('.windows-') }
-versions.reject! { |version| version[:url].end_with?('.msi') }
-versions.reject! { |version| version[:url].end_with?('.pkg') }
-versions.reject! { |version| version[:url].end_with?('.src.tar.gz') }
-versions.uniq! { |version| version[:url] }
-
-FileUtils.rm_rf("#{root}/db")
-
-versions.each do |version|
-  # https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz
-  match = version[:url].scan(%r{ ^https://dl.google.com/go/go(\d+\.\d+(?:\.\d+)?)\.(\w+)-(\w+).tar.gz$ }x)
+  match = url.scan(%r{ ^https://dl.google.com/go/go(\d+\.\d+(?:\.\d+)?)\.(\w+)-(\w+).tar.gz }x)
   next unless match
 
   ver, os, arch = match.first
@@ -53,7 +42,76 @@ versions.each do |version|
   arch = 'i686' if arch == '386'
   arch = 'x86_64' if arch == 'amd64'
 
-  FileUtils.mkdir_p("#{root}/db/#{ver}/#{os}/#{arch}")
-  File.write("#{root}/db/#{ver}/#{os}/#{arch}/url", version[:url])
-  File.write("#{root}/db/#{ver}/#{os}/#{arch}/checksum", version[:checksum])
+  checksum = tr.xpath('./td/tt/text()').first.to_s
+  next unless checksum
+
+  {
+    ver:      ver,
+    os:       os,
+    arch:     arch,
+    url:      url,
+    checksum: checksum,
+  }
+end
+new_versions.reject!(&:nil?)
+
+new_versions.reject! { |new_version| new_version[:url].include?('-bootstrap-') }
+new_versions.reject! { |new_version| new_version[:url].include?('.windows-') }
+new_versions.reject! { |new_version| new_version[:url].end_with?('.msi') }
+new_versions.reject! { |new_version| new_version[:url].end_with?('.pkg') }
+new_versions.reject! { |new_version| new_version[:url].end_with?('.src.tar.gz') }
+new_versions.uniq! { |new_version| new_version[:url] }
+new_versions.sort_by! { |new_version| Gem::Version.new(new_version[:ver]) }
+
+old_versions = Dir.glob("#{root}/db/*/*/*/")
+old_versions.map! do |dir|
+  {
+    ver:      File.basename(File.dirname(File.dirname(dir))),
+    os:       File.basename(File.dirname(dir)),
+    arch:     File.basename(dir),
+    url:      File.read("#{dir}/url"),
+    checksum: File.read("#{dir}/checksum"),
+  }
+end
+old_versions.sort_by! { |old_version| Gem::Version.new(old_version[:ver]) }
+
+msg('Removing old versions ...', $red) { FileUtils.rm_rf("#{root}/db") }
+
+new_versions.each do |new_version|
+  existing = old_versions.find do |old_version|
+    old_version[:ver] == new_version[:ver] &&
+      old_version[:os] == new_version[:os] &&
+      old_version[:arch] == new_version[:arch]
+  end
+  same = old_versions.find do |old_version|
+    old_version[:ver] == new_version[:ver] &&
+      old_version[:os] == new_version[:os] &&
+      old_version[:arch] == new_version[:arch] &&
+      old_version[:url] == new_version[:url] &&
+      old_version[:checksum] == new_version[:checksum]
+  end
+
+  old_versions.delete_if do |old_version|
+    old_version[:ver] == new_version[:ver] &&
+      old_version[:os] == new_version[:os] &&
+      old_version[:arch] == new_version[:arch]
+  end
+
+  message, color = if same
+    ["Keep    #{new_version[:ver]}/#{new_version[:os]}/#{new_version[:arch]}", $reset]
+  elsif existing
+    ["Freshen #{new_version[:ver]}/#{new_version[:os]}/#{new_version[:arch]}", $yellow]
+  else
+    ["Create  #{new_version[:ver]}/#{new_version[:os]}/#{new_version[:arch]}", $green]
+  end
+
+  msg(message, color) do
+    FileUtils.mkdir_p("#{root}/db/#{new_version[:ver]}/#{new_version[:os]}/#{new_version[:arch]}")
+    File.write("#{root}/db/#{new_version[:ver]}/#{new_version[:os]}/#{new_version[:arch]}/url", new_version[:url])
+    File.write("#{root}/db/#{new_version[:ver]}/#{new_version[:os]}/#{new_version[:arch]}/checksum", new_version[:checksum])
+  end
+end
+
+old_versions.each do |old_version|
+  msg("Delete  #{old_version[:ver]}/#{old_version[:os]}/#{old_version[:arch]}", $red)
 end
